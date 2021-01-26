e65af7dd70b91d8ad0622e82178f8e00be588d21
Badruljama <bjjstr8721@gmail.com>
2021-01-26 01:21:10 -0800

    complete form

---
+++ login.js
@@ -0,0 +1,13 @@
+
+function validate(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if(username=="admin"&& password=="user"){
+        alert("login succesfully");
+        return false;
+    }
+    else{
+        alert("login failed");
+    }
+}
